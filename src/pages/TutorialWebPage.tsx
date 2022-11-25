9c31a32239b32b29de9767931348462b9a240538
hibye0714 <48339717+YongJian-YJ@users.noreply.github.com>
2022-11-25 22:23:11 +0800

    .

--- src/pages/TutorialWebPage.tsx
+++ src/pages/TutorialWebPage.tsx
@@ -1,54 +1,31 @@
 import { FunctionComponent } from "react";
 import styles from "./TutorialWebPage.module.css";
+import { useNavigate } from "react-router-dom";
 
 const TutorialWebPage: FunctionComponent = () => {
+  let navigate = useNavigate(); 
+  const handleMenuClick = () => window.open("https://yongjian-yj.github.io/reactProject/", "_blank");
+
   return (
     <div className={styles.tutorialWebPage}>
-      <div className={styles.kryptoDiv}>krypto</div>
-      <div className={styles.joinOurNewsletter}>Join our newsletter</div>
-      <div className={styles.homeAboutBuyNFTsSellNFTs}>
-        <p className={styles.homeP}>Home</p>
-        <p className={styles.homeP}>About</p>
-        <p className={styles.homeP}>Buy NFTs</p>
-        <p className={styles.sellNFTsP}>Sell NFTs</p>
-      </div>
-      <div className={styles.browseNFTsBuyNFTsSellNFTs}>
-        <p className={styles.homeP}>Browse NFTs</p>
-        <p className={styles.homeP}>Buy NFTs</p>
-        <p className={styles.sellNFTsP}>Sell NFTs</p>
-      </div>
-      <div className={styles.emailLinkedInInstagramTwitt}>
-        <p className={styles.homeP}>Email</p>
-        <p className={styles.homeP}>LinkedIn</p>
-        <p className={styles.homeP}>Instagram</p>
-        <p className={styles.sellNFTsP}>Twitter</p>
-      </div>
-      <div className={styles.kryptoDiv1}>krypto</div>
-      <div className={styles.marketDiv}>Market</div>
-      <div className={styles.contactDiv}>contact</div>
-      <div className={styles.rectangleDiv} />
-      <div className={styles.buttonDiv}>
-        <div className={styles.submitDiv}>submit</div>
-      </div>
-      <div className={styles.emailAddressDiv}>Email Address</div>
       <div className={styles.getStatedDiv}>
         <div className={styles.rectangleDiv1} />
         <div className={styles.rectangleDiv2} />
         <div className={styles.areYouReady}>are you ready?</div>
         <b className={styles.beAPartOfTheNextBigThin}>
-          <p className={styles.homeP}>be a part of the</p>
-          <p className={styles.sellNFTsP}>next big thing</p>
+          <p className={styles.homeP}>Win the next rank games</p>
+          <p className={styles.sellNFTsP}>Impress your teammate!</p>
         </b>
         <div className={styles.buttonDiv1}>
           <div className={styles.submitDiv}>get started</div>
         </div>
       </div>
       <div className={styles.navBarDiv}>
-        <b className={styles.kryptoB}>krypto</b>
+        <b className={styles.kryptoB}>ML_Trainer</b>
         <div className={styles.aboutDiv}>about</div>
         <div className={styles.pricingDiv}>pricing</div>
         <div className={styles.contactDiv1}>contact</div>
-        <div className={styles.buyNFTDiv}>buy NFT</div>
+        <div className={styles.buyNFTDiv}>Trainers</div>
       </div>
       <div className={styles.tesimonialSectionDiv}>
         <div className={styles.tESTIMONIALDiv}>TESTIMONIAL</div>
@@ -66,7 +43,7 @@ const TutorialWebPage: FunctionComponent = () => {
           <div className={styles.oliviaColeDiv}>olivia cole</div>
           <div
             className={styles.loremIpsumDolorSitAmetCo}
-          >{`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ullamcorper scelerisque mi, in malesuada felis malesuada vel. `}</div>
+          >{`With the help of those trainers, I have forgotten when was the last time being called 'epic kontol' `}</div>
         </div>
         <div className={styles.testimonialItemDiv1}>
           <div className={styles.rectangleDiv3} />
@@ -78,7 +55,7 @@ const TutorialWebPage: FunctionComponent = () => {
           <div className={styles.oliviaColeDiv}>evan White</div>
           <div
             className={styles.loremIpsumDolorSitAmetCo}
-          >{`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ullamcorper scelerisque mi, in malesuada felis malesuada vel. `}</div>
+          >{`Reaching legend is not a dream anymore, I am confident that I will be able to step through the gate of mythic one day `}</div>
         </div>
         <div className={styles.testimonialItemDiv2}>
           <div className={styles.rectangleDiv3} />
@@ -90,7 +67,7 @@ const TutorialWebPage: FunctionComponent = () => {
           <div className={styles.oliviaColeDiv}>Jessica Page</div>
           <div
             className={styles.loremIpsumDolorSitAmetCo}
-          >{`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ullamcorper scelerisque mi, in malesuada felis malesuada vel. `}</div>
+          >{`The Kagura skill of one of the trainer has seriously impressed me! Wow..just Wow.. `}</div>
         </div>
       </div>
       <div className={styles.sectionGetOurApp}>
@@ -117,25 +94,18 @@ const TutorialWebPage: FunctionComponent = () => {
       </div>
       <div className={styles.sectionAnalyticsDiv}>
         <div className={styles.objectOther18Div}>
-          <img
-            className={styles.objectOther17Icon}
-            alt=""
-            src={require('./images/objectother-18@2x.png')}
-          />
+        <iframe width="500" height="315" src="https://www.youtube.com/embed/MK8ss8JLPO8" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </div>
         <div className={styles.buttonDiv3}>
           <div className={styles.submitDiv}>View our pricing</div>
         </div>
         <div className={styles.useOurBuiltInAnalyticsDas}>
-          Use your built-in analytics dashboard to pull valuable insights and
-          monitor the value of your Krypto portfolio over time.
+          We believe the best way to learn is through observing how others applying the hero's capabilities to the maximum.
         </div>
         <b className={styles.builtInAnalyticsToTrackYo}>
           <span
             className={styles.browseSpan}
-          >{`built-in analytics to track your `}</span>
-          <span className={styles.nftSpan}>nft</span>
-          <span className={styles.browseSpan}>s</span>
+          >{`Fantastic videos from our trainers `}</span>
         </b>
         <div className={styles.aNALYTICSDiv}>ANALYTICS</div>
       </div>
@@ -157,19 +127,19 @@ const TutorialWebPage: FunctionComponent = () => {
           />
         </div>
         <b className={styles.discoverAndCollectRareNFTs}>
-          discover and collect rare NFTs
+          Wondering why you suck in Mobile Legends?
         </b>
         <div className={styles.theMostSecureMarketplaceFo}>
-          <p className={styles.homeP}>The most secure marketplace for buying</p>
-          <p className={styles.sellNFTsP}>and selling unique crypto assets.</p>
+          <p className={styles.homeP}>Tired of being called "Epic kontol"?</p>
+          <p className={styles.sellNFTsP}>Fred not...We are here to help :)</p>
         </div>
         <div className={styles.bUYNFTSDiv}>
           <div className={styles.rectangleDiv7} />
-          <div className={styles.buyNftsDiv}>buy nfts</div>
+          <div onClick={handleMenuClick} className={styles.buyNftsDiv}>Watch and Learn</div>
         </div>
         <div className={styles.sellNFTSDiv}>
           <div className={styles.rectangleDiv8} />
-          <div className={styles.sellNftsDiv}>sell nfts</div>
+          <div className={styles.sellNftsDiv}>Hero Ranking</div>
         </div>
       </div>
     </div>
